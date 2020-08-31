6e8935e1a4f8cc62386bf38e97e53cf2bdff1947
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-08-31 20:13:14 +0300

    #3 Server (start settings)

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const express = require('express');
+// babel
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import history from 'connect-history-api-fallback';
+import path from 'path';
 
 const app = express();
 
+// Middlewares
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload({useTempFiles: true}));
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname);
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port ' + app.get('port'));
 })
